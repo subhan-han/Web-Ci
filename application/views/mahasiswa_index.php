f3a8521f86eec288a45044647fed02bf45824bd2
subhan-han <juliosubhan@gmail.com>
2020-11-12 13:05:32 +0800

    update bootstrap

--- application/views/mahasiswa_index.php
+++ application/views/mahasiswa_index.php
@@ -2,25 +2,51 @@
 <html>
 <head>
 	<title>mahasiswa</title>
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/bootstrap.min.css') ?>">
 </head>
 <body>
-<h1>mahasiswa</h1>
-<h3>selamat datang <?= $nama_lengkap ?></h3>
-<p>nim saya <?= $nim ?></p>
-<hr>
-<a href="<?= site_url('mahasiswa/tambah')?>">tambah</a>
-<table border="1">
-	<tr>
-		<th>NIM</th>
-		<th>NAMA</th>
-	</tr>
-	<?php foreach ($mahasiswa as $key ):?> 
-		<tr>
-			<td><?= $key['nim']?></td>
-			<td><?= $key['nama']?></td>
+	<nav class="navbar navbar-expand-lg navbar-dark shadow" style="background-color: #000000">
+		<a href="" class="navbar-brand">
+			Tugas
+		</a>
+		<div class="navbar-nav">
+			<a href="<?= site_url('mahasiwa')?>" class="nav-link active">HOME</a>
+			<a href="<?= site_url('mahasiswa/tambah')?>" class="nav-link">Tambah Data</a>
+		</div>
+		<div class="navbar-nav ml-auto">
+			<a href="" class="nav-link active">subhan</a>
+			
+		</div>
+	</nav>
+	<div class="alert alert-info text-center mt-2 shadow">
+		Data Mahasiswa
+	</div>
+<div class="container">
+<table class="table table-bordered table-striped table-hover ">
+		
+		<tr class="text-center">
+			<th>NO</th>
+			<th>NIM</th>
+			<th>Nama</th>
+			<th>Alamat</th>
+			<th>Aksi</th>
 		</tr>
-	<?php endforeach?>
-</table>
-<?= $mahasiswa[0]['nim']?>
+		<tr>
+			<td>1</td>
+			<td>1810310001</td>
+			<td>subhan</td>
+			<td>Mataram</td>
+			<td class="text-center">
+				
+				<a href="" class="btn btn-info btn-sm">EDIT</a>
+				<a href="" class="btn btn-success btn-sm">EDIT</a>
+
+
+			</td>
+		</tr>
+
+	</table>
+</div>
+
 </body>
 </html>
--- application/views/mahasiswa_tambah.php
+++ application/views/mahasiswa_tambah.php
@@ -2,18 +2,25 @@
 <html>
 <head>
 	<title>Mahasiswa</title>
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/bootstrap.min.css') ?>">
 </head>
 <body>
-	<h1>Tambah Mahasiswa</h1>
-	<a href="<?= site_url('mahasiswa')?>">kembali</a>
+	<a class="btn btn-Danger btn-sm float-right text-dark " href="<?= site_url('mahasiswa')?>">kembali</a>
+	<div class="p-5 mb-7 bg-info text-white"><b>	TAMBAH MAHASISWA</b>
 <hr>
+		<div class="col-md-4">
 <form>
-	<h6>NIM</h6>
-		<input type="number" name="nim"> <br>
-		<h6>Nama Mahasiswa</h6>
-		<input type="text" name="nama_mahasiswa"><br>
+		<div class=" form-group col-md-4">
+		<label>NIM</label>
+		<input type="text" name="nim" > <br>
 
-		<input type="submit" name="submit" value="simpan">
+		<label>Nama mahasiswa</label>
+		<input type="text" name="nama_mahasiswa"><br>
+		<label>Alamat</label>
+		<input type="text" name="alamat_mahasiswa"><br>
+
+		<input type="submit" name="submit" value="simpan" class="btn btn-info btn-sm float-right">
 </form>
+</div>
 </body>
 </html>
